76f163542fa44a4beb0f1a0a1661ec32cee95e94
justice9 <41146348-justice9@users.noreply.replit.com>
2025-04-05 17:29:37 +0000

    Restored to 'd7e280d7f8b6a2cdf4e9bff32b9e825464f3e907'
    Replit-Restored-To: d7e280d7f8b6a2cdf4e9bff32b9e825464f3e907

--- client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Apply default font family
-document.documentElement.classList.add("font-serif");
-
-createRoot(document.getElementById("root")!).render(<App />);
